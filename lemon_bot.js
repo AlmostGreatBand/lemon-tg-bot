e0adbceeee939d3ac5ea4ae792163b9c7f7c4fe8
DjBee0312 <pashapasha0312@gmail.com>
2021-01-09 17:17:55 +0200

    feat(bot): added menu and card demonstration

--- lemon_bot.js
+++ lemon_bot.js
@@ -1,14 +1,35 @@
 'use strict';
 const Telegraf = require('telegraf');
-const Markup = require('telegraf/markup')
-const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup');
+const Extra = require('telegraf/extra');
 
 const token = process.env.LEMON_TELEGRAM_TOKEN;
 
 const bot = new Telegraf(token);
 
+const cards = [4242424242424242,4003830171874018,2223007648726984,2222990905257051];
+let currentCardNumber;
+
+const cardButtonsGenerator = (cardsArr) => {
+    const result = [];
+    cardsArr.forEach((cardNumber, index) => {
+        const cardData = `card${index}`;
+        const cardNumberStr = cardNumber.toString()
+        const cardButtonText = cardNumberStr.substr(0,2) + '...' + cardNumberStr.substr(12,15)
+        const cardButton = Markup.callbackButton(cardButtonText, cardData)
+        result.push(cardButton);
+
+        bot.action(cardData,(ctx) => {
+            ctx.editMessageText(`CardNumber: ${cardNumber}`);
+        })
+    })
+    return result;
+}
+
+const cardsButtons = cardButtonsGenerator(cards);
+
 //Start bot
-bot.start((ctx) => ctx.reply('Welcome to Lemon telegram bot'))
+bot.start((ctx) => ctx.reply('Welcome to Lemon telegram bot! To start please send \'menu\''))
 
 //Reaction on command /help
 bot.help((ctx) => ctx.reply('This bot is in development right now'))
@@ -19,6 +40,12 @@ bot.on('sticker', (ctx) => ctx.reply('Nice sticker'))
 //Reply on message Marcus Aurelius
 bot.hears('Marcus Aurelius', (ctx) =>
     ctx.reply('You have power over your mind - not outside events. Realize this, and you will find strength.'))
+
+bot.hears('menu', (ctx) =>{
+    ctx.reply('Hello <b>Username</b>. <i>Please, choose a card</i>',
+        Extra.HTML()
+            .markup(Markup.inlineKeyboard(cardsButtons)))
+})
 
 //Reply on message testing
 bot.hears('testing', (ctx) => {
